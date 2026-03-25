8ef68fd3c700c7467043c23172ae2935f73b86c1
mohammed73nabil-beep <mohammed.73.nabil@gmail.com>
2026-03-25 23:57:27 +0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -7,7 +7,7 @@ const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'my_website_db',
+  database: process.env.DB_NAME || 'test',
   port: Number(process.env.DB_PORT) || 4000, // TiDB يستخدم منفذ 4000 عادةً
   waitForConnections: true,
   connectionLimit: 10,
